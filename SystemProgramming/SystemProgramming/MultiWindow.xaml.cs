41159b2ab09624300838f0df9583eabc9995cb7a
AntonDegt <108671823+AntonDegt@users.noreply.github.com>
2023-03-06 22:43:37 +0300

    Add files via upload

---
+++ SystemProgramming/SystemProgramming/MultiWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SystemProgramming
+{
+    /// <summary>
+    /// Логика взаимодействия для MultiWindow.xaml
+    /// </summary>
+    public partial class MultiWindow : Window
+    {
+        public MultiWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void ButtonStart1_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                
+            }
+        }
+
+        private void ButtonStop1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private double sum;
+        private int precent;
+        private Random random = new Random();
+
+        private void plusPrecent()
+        {
+            double val = sum;
+            
+            Thread.Sleep(random.Next(250, 350));
+            
+            val *= 1 + precent / 100;
+
+            sum = val;
+
+            progressBar1.Value += 10;
+
+            Dispatcher.Invoke(() => ConsoleBlock.Text += sum + "\n");
+        }
+    }
+}
---
+++ SystemProgramming/SystemProgramming/ThreadingWindow.xaml.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SystemProgramming
+{
+    /// <summary>
+    /// Логика взаимодействия для ThreadingWindow.xaml
+    /// </summary>
+    public partial class ThreadingWindow : Window
+    {
+        public ThreadingWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        #region part 1 - "зависание интерфейса"
+        private void ButtonStart1_Click(object sender, RoutedEventArgs e)
+        {
+            Start1();
+        }
+
+        private void ButtonStop1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Start1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                progressBar1.Value = (i + 1) * 10;
+                ConsoleBlock.Text += i.ToString() + "\n";  // Console.WriteLine(i);
+                Thread.Sleep(300);
+            }
+        }
+        #endregion
+
+        #region part 2 - Исключение (обращение к другому потоку)
+        private void ButtonStart2_Click(object sender, RoutedEventArgs e)
+        {
+            new Thread(Start1).Start();  //  - исключение (изменения из другого потока)
+        }
+
+        private void ButtonStop2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region part 3 - Решение
+        private void ButtonStart3_Click(object sender, RoutedEventArgs e)
+        {
+            isStopped = false;
+            new Thread(Start3).Start();
+        }
+
+        private void ButtonStop3_Click(object sender, RoutedEventArgs e)
+        {
+            isStopped = true;
+        }
+        private bool isStopped;  // "общая" переменная, доступная всем потокам
+        private void Start3()
+        {
+            for (int i = 0; i < 10 && !isStopped; i++)
+            {
+                this.Dispatcher.Invoke(() =>  // делегирование работы потоку окна
+                {
+                    progressBar3.Value = (i + 1) * 10;
+                    ConsoleBlock.Text += i.ToString() + "\n";  // Console.WriteLine(i);
+                });
+
+                Thread.Sleep(300);
+            }
+        }
+        #endregion
+
+        #region part 4 - Взаимодействие потоков (продолжение прогресса)
+        private void ButtonStart4_Click(object sender, RoutedEventArgs e)
+        {
+            isStopped4 = false;
+            // параметр для Start4 передается в вызов .Start(0)
+            new Thread(Start4).Start(savedIndex4);
+            if (savedIndex4 == 0)
+            {
+                ConsoleBlock.Text = "";
+            }
+        }
+        private void ButtonStop4_Click(object sender, RoutedEventArgs e)
+        {
+            isStopped4 = true;
+        }
+        private bool isStopped4;
+        private int savedIndex4;  // при прерывании потока - храним последний i
+        private void Start4(object? startIndex)
+        {
+            // при приеме параметра обязательно преобразовать его к
+            // ожидаемому типу (и проверить на валидность)
+            if (startIndex is int startFrom)
+            {
+                for (int i = startFrom; i < 10; i++)
+                {
+                    if (isStopped4)
+                    {
+                        savedIndex4 = i;
+                        return;
+                    }
+                    this.Dispatcher.Invoke(() =>  // делегирование работы потоку окна
+                    {
+                        progressBar4.Value = (i + 1) * 10;
+                        ConsoleBlock.Text += i.ToString() + "\n";
+                    });
+
+                    Thread.Sleep(300);
+                }
+                // если поток закончился (без прерываний) 
+                savedIndex4 = 0;
+            }
+        }
+        #endregion
+
+        #region part 5 - Д/З - Блокировать возможности запустить новый поток пока не закончит работу предыдущий
+        /* Д.З. Реализовать блокироку возможности запустить новый поток
+         * пока не закончит работу предыдущий (в т.ч. прерыванием)
+         * а) disable кнопки
+         *   либо
+         * б) сохранять ссылку на поток:  thread = new Thread(...)
+         *    и проверять ее на null
+         * Если поток прерывается (не закончившись) менять надпись
+         * кнопки Start на Resume. По окончанию - Start
+         */
+        private void ButtonStart5_Click(object sender, RoutedEventArgs e)
+        {
+            isStopped5 = false;
+
+            ButtonStop5.IsEnabled = true;
+            ButtonStart5.IsEnabled = false;
+
+            new Thread( Start5 ).Start(savedIndex5);
+            if (savedIndex5 == 0)
+            {
+                ConsoleBlock.Text = "";
+            }
+        }
+        private void ButtonStop5_Click(object sender, RoutedEventArgs e)
+        {
+            isStopped5 = true;
+
+            ButtonStop5.IsEnabled = false;
+            ButtonStart5.IsEnabled = true;
+            ButtonStart5.Content = "Resume";
+        }
+
+        private void End5()
+        {
+            ButtonStop5.IsEnabled = false;
+            ButtonStart5.IsEnabled = true;
+            ButtonStart5.Content = "Start";
+        }
+
+        private bool isStopped5;
+        private int savedIndex5;
+
+        private void Start5(object? startIndex)
+        {
+
+            if (startIndex is int startFrom)
+            {
+                for (int i = startFrom; i < 10; i++)
+                {
+                    if (isStopped5)
+                    {
+                        savedIndex5 = i;
+                        return;
+                    }
+                    this.Dispatcher.Invoke(() =>  // делегирование работы потоку окна
+                    {
+                        progressBar5.Value = (i + 1) * 10;
+                        ConsoleBlock.Text += i.ToString() + "\n";
+                    });
+
+                    Thread.Sleep(300);
+                }
+                // если поток закончился (без прерываний) 
+                savedIndex5 = 0;
+                Dispatcher.Invoke(() => End5());
+            }
+        }
+        #endregion
+    }
+}
